6f9678462d35b1149cc1bda441237ca6e4184510
SoponSufi <SoponSufi@DESKTOP-T52DVE1>
2024-12-20 22:59:35 +0530

    Add project files.

---
+++ DiamondNumber/Controllers/NumberController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiamondNumber.Controllers
+{
+    public class NumberController : Controller
+    {
+        // GET: Number
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
